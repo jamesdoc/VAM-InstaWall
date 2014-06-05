4621e07fa76fe7837b9411333cddc4144e3da1ea
James Doc <jamesdoc@gmail.com>
2014-06-05 11:00:09 +0100

    Added config template

---
+++ config_template.py
@@ -0,0 +1,14 @@
+## Instagram client id
+client_id = ''
+
+## Instagram client secret
+client_src = ''
+
+## Facebook location id to import
+location_id = ''
+
+## Instagram user id to import
+user_id = ''
+
+# List of tags to import.
+tags = ['tree', 'butterfly', 'horse']
